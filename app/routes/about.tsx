d777b5d9de8954f77c7ccdc5957084941f8d6f6d
zk <zk2840174@gmail.com>
2025-04-12 11:25:09 +0900

    aa

---
+++ app/routes/about.tsx
@@ -0,0 +1,12 @@
+
+
+
+function AboutPage() {
+
+    return (
+        <div>
+            <div className={'text-4xl bg-amber-400 m-2 p-2 border-2 rounded'}>About Pge</div>
+        </div>
+    )
+}
+export default AboutPage
